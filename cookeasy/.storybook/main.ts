039b8a151ef0f914f66e83afaede6ce2d2705724
Santiago Benito <bmsanti@hotmail.com>
2024-08-13 12:03:50 +0200

    Estructure prepared

---
+++ cookeasy/.storybook/main.ts
@@ -0,0 +1,28 @@
+module.exports = {
+  stories: ["../src/ui/components/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@chromatic-com/storybook",
+  ],
+  framework: "@storybook/react-vite",
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.(png|jpe?g|gif|svg)$/i,
+      use: [
+        {
+          loader: "file-loader",
+          options: {
+            name: "[name].[ext]",
+            outputPath: "assets/images",
+          },
+        },
+      ],
+    });
+
+    return config;
+  },
+  typescript: {
+    reactDocgen: "react-docgen-typescript",
+  },
+};
